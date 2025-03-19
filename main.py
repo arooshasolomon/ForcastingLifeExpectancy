c5ed8f05e387de55ed6db967747951018d9840ac
Chai <zdc4tp@virginia.edu>
2025-03-19 11:52:55 -0400

    Added to main.py

--- main.py
+++ main.py
@@ -2,3 +2,9 @@
 TO-DO:
 - a script to run the full pipeline from start to finish (load data, preprocess, trains models, evaluates results)
 '''
+
+from scripts.data_preprocessing import main as preprocess_data
+
+if __name__ == "__main__":
+    preprocess_data()  
+    # placeholder for calling modeling scripts
